c8e6d2a34bae2deb19d000a9b99565e4d872c7e7
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-11-03 23:15:18 +0530

    Updates

---
+++ precache-manifest.fd02931bc9895df6f7c9fd26ca0cc219.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2794a413c457cfc5b20",
+    "url": "/airplanes/static/js/runtime~main.c2794a41.js"
+  },
+  {
+    "revision": "548b7adc1ea746f476b2",
+    "url": "/airplanes/static/js/main.548b7adc.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/airplanes/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "548b7adc1ea746f476b2",
+    "url": "/airplanes/static/css/main.99323c2e.chunk.css"
+  },
+  {
+    "revision": "9a400cbbca9bae281d7f5bf3c7cac486",
+    "url": "/airplanes/index.html"
+  }
+];
